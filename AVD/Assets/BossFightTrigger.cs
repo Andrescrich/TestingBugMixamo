7ac6fd7189d86674e28754201ed34c392306625b
André <andrescrich@hotmail.com>
2020-04-27 21:35:08 +0200

    1.0

--- AVD/Assets/BossFightTrigger.cs
+++ AVD/Assets/BossFightTrigger.cs
@@ -1,13 +1,29 @@
-﻿using UnityEngine;
+﻿using System;
+using CreatorKitCode;
+using CreatorKitCodeInternal;
+using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 public class BossFightTrigger : MonoBehaviour
 {
     private PlayableDirector pd;
+    public PlayableAsset endingAsset;
     private bool activated;
+    public GameObject boss;
     void Awake()
     {
         pd = GetComponent<PlayableDirector>();
+    }
+
+    private void Update()
+    {
+        if(boss.GetComponent<SimpleEnemyController>() == null)
+        {
+            pd.playableAsset = endingAsset;
+            pd.Play();
+            enabled = false;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
